e3302446217dbc56770cc46e565060f1ea33d2c0
sayed zahir <kabul_zahir@yahoo.com>
2024-12-26 13:03:07 -0500

    front end created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  return (
+   <>
+   <h1 className="text-success">First </h1>
+   </>
+  );
+}
+
+export default App;
